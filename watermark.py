a3fe38d966c0c8c0468e01fb2eac2d2f7c885bea
peacebefore <peacebefore@gmail.com>
2023-10-11 11:31:35 -0700

    finalizing watermark functionality

--- watermark.py
+++ watermark.py
@@ -7,3 +7,7 @@ output = PyPDF2.PdfFileWriter()
 for i in range(template.getNumPages()):
     page = template.getPage(i)
     page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
